a76474a546fe97d39690e180630b596409e156be
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-03 17:33:47 +0300

    Create weather.dart

---
+++ lib/domain/entities/weather.dart
@@ -0,0 +1,19 @@
+class Weather {
+  final int id;
+  final String cityName;
+  final String main;
+  final String description;
+  final double temp;
+  final int pressure;
+  final int humidity;
+
+  Weather(
+    this.id,
+    this.cityName,
+    this.main,
+    this.description,
+    this.temp,
+    this.pressure,
+    this.humidity,
+  );
+}
