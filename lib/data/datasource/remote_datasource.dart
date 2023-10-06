514fe3f8e8b615f1f98ff33d17a85086df76dc4d
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-06 15:00:35 +0300

    Create remote_datasource.dart

---
+++ lib/data/datasource/remote_datasource.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:weather/core/utils/constants.dart';
+import 'package:weather/data/models/weather_model.dart';
+import 'package:dio/dio.dart';
+
+abstract class BaseRemoteDataSource {
+  Future<WeatherModel?> getWeatherByCityName(String cityName);
+}
+
+class RemoteDataSource implements BaseRemoteDataSource {
+  @override
+  Future<WeatherModel?> getWeatherByCityName(String cityName) async {
+    try {
+      var response = await Dio().get(
+          '${AppConstants.baseUrl}/weather?q=$cityName&appid=${AppConstants.appKey}');
+      log('$response');
+      return WeatherModel.fromJson(
+        response.data,
+      );
+    } catch (e) {
+      log('$e');
+      return null;
+    }
+  }
+}
