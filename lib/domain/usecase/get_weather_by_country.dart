7db18daf5642a5bc2477015d3531bc55524e6826
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-07 07:17:49 +0300

    Update get_weather_by_country.dart

--- lib/domain/usecase/get_weather_by_country.dart
+++ lib/domain/usecase/get_weather_by_country.dart
@@ -1,10 +1,10 @@
 import 'package:weather/domain/entities/weather.dart';
 import 'package:weather/domain/repositroy/base_weather_repository.dart';
 
-class GetWeatherByCountry {
+class GetWeatherByCity {
   final BaseWeatherRepository repository;
 
-  GetWeatherByCountry(this.repository);
+  GetWeatherByCity(this.repository);
 
   Future<Weather> execute(String cityName) async {
     return await repository.getWeatherByCityName(cityName);
