a5c26b850f84e9914a9b01016088904da5e72af1
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-10-07 08:46:55 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:weather/data/datasource/remote_datasource.dart';
 import 'package:weather/data/repository/weather_repository.dart';
 import 'package:weather/domain/entities/weather.dart';
@@ -32,19 +33,55 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(),
         body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.purple,
+                Colors.deepPurple,
+              ],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+            ),
+          ),
           height: double.infinity,
           width: double.infinity,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text(
-                weather.cityName,
-                textAlign: TextAlign.center,
+              Card(
+                color: Colors.white24,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            weather.cityName,
+                            textAlign: TextAlign.left,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Text(
+                      weather.temp.toStringAsFixed(1),
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
+                      ),
+                    ),
+                  ],
+                ),
               ),
               Text(weather.description),
               Text(weather.main),
-              Text(weather.temp.toString()),
               Text(weather.humidity.toString()),
             ],
           ),
