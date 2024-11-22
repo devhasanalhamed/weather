aeb397b356911e72c95ee1f35cb8874dfdf84056
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-11-22 07:13:43 +0300

    Update weather_provider.dart

--- lib/weather/presentation/controllers/weather_provider.dart
+++ lib/weather/presentation/controllers/weather_provider.dart
@@ -1,25 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:weather/weather/data/datasource/remote_data_source.dart';
-import 'package:weather/weather/data/repository/weather_repository.dart';
+import 'package:weather/core/enums/request_state.dart';
+import 'package:weather/core/enums/weather_scale.dart';
 import 'package:weather/weather/domain/entities/weather.dart';
-import 'package:weather/weather/domain/repository/base_weather_repository.dart';
 import 'package:weather/weather/domain/usecase/get_weather_by_city_name.dart';
 
 class WeatherProvider extends ChangeNotifier {
-  late BaseRemoteDataSource baseRemoteDataSource;
-  late BaseWeatherRepository baseWeatherRepository;
+  final GetWeatherByCityName getWeatherByCityNameUseCase;
+  WeatherProvider({
+    required this.getWeatherByCityNameUseCase,
+  });
 
-  WeatherProvider() {
-    initialize();
-  }
-
-  Future<void> initialize() async {
-    baseRemoteDataSource = RemoteDataSource();
-    baseWeatherRepository =
-        WeatherRepository(baseRemoteDataSource: baseRemoteDataSource);
-  }
-
+  late Weather weatherModel;
+  RequestState weatherRequestState = RequestState.loading;
   Future<Weather> getWeatherByCityName(String cityName) async {
-    return await GetWeatherByCityName(baseWeatherRepository).execute("riyadh");
+    weatherRequestState = RequestState.loading;
+    try {
+      weatherModel = await getWeatherByCityNameUseCase.execute(cityName);
+      weatherRequestState = RequestState.done;
+    } catch (error) {
+      weatherRequestState = RequestState.error;
+    }
+    notifyListeners();
+    return weatherModel;
+  }
+
+  WeatherScale weatherScale = WeatherScale.kelvin;
+  String get temperatureWithScale {
+    if (weatherScale == WeatherScale.kelvin) {
+      return "${weatherModel.tempK.toStringAsFixed(1)}K";
+    }
+    if (weatherScale == WeatherScale.fahrenheit) {
+      return "${weatherModel.tempF.toStringAsFixed(1)}°F";
+    }
+    return "${weatherModel.tempC.toStringAsFixed(1)}°C";
+  }
+
+  void changeWeatherScale(WeatherScale scale) {
+    if (weatherScale != scale) {
+      weatherScale = scale;
+      notifyListeners();
+    }
   }
 }
