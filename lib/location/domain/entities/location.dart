76b9e9586bf566bdb7eb28546e5d73925b3ad49e
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-11-23 06:34:26 +0300

    Create location.dart

---
+++ lib/location/domain/entities/location.dart
@@ -0,0 +1,11 @@
+class Location {
+  final String cityName;
+  final double latitude;
+  final double longitude;
+
+  Location({
+    required this.cityName,
+    required this.latitude,
+    required this.longitude,
+  });
+}
