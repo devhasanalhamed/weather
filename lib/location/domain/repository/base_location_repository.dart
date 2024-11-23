243ef1d82c7dbda415f3385e237a3a79b5fd4acd
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-11-23 06:34:28 +0300

    Create base_location_repository.dart

---
+++ lib/location/domain/repository/base_location_repository.dart
@@ -0,0 +1,11 @@
+import 'package:weather/location/domain/entities/location.dart';
+
+abstract class BaseLocationRepository {
+  Future<Location> getLocation();
+
+  Future<void> setLocation(
+    String? cityName,
+    double? latitude,
+    double? longitude,
+  );
+}
