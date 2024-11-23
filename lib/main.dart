a29a4753d38be4a7b15e2bbc467aa5175d87ba65
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-11-23 06:34:39 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
+import 'package:weather/location/data/datasource/location_data_source.dart';
+import 'package:weather/location/data/repository/location_repository.dart';
+import 'package:weather/location/domain/repository/base_location_repository.dart';
+import 'package:weather/location/domain/usecases/get_location.dart';
+import 'package:weather/location/domain/usecases/set_location.dart';
+import 'package:weather/location/presentation/controllers/location_provider.dart';
+import 'package:weather/location/presentation/presentation/location_screen.dart';
 import 'package:weather/weather/data/datasource/remote_data_source.dart';
 import 'package:weather/weather/data/repository/weather_repository.dart';
 import 'package:weather/weather/domain/repository/base_weather_repository.dart';
 import 'package:weather/weather/domain/usecase/get_weather_by_city_name.dart';
-import 'package:weather/weather/presentation/components/weather_card.dart';
 import 'package:weather/weather/presentation/controllers/weather_provider.dart';
 
 void main() async {
@@ -18,12 +24,26 @@ void main() async {
   GetWeatherByCityName getWeatherByCityName =
       GetWeatherByCityName(baseWeatherRepository);
 
+  BaseLocationDataSource baseLocationDataSource = LocationDataSource();
+  BaseLocationRepository baseLocationRepository =
+      LocationRepository(baseLocationDataSource: baseLocationDataSource);
+  GetLocationUseCase getLocationUseCase =
+      GetLocationUseCase(baseLocationRepository: baseLocationRepository);
+  SetLocationUseCase setLocationUseCase =
+      SetLocationUseCase(baseLocationRepository: baseLocationRepository);
+
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(
           create: (context) => WeatherProvider(
             getWeatherByCityNameUseCase: getWeatherByCityName,
+          ),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => LocationProvider(
+            getLocationUseCase: getLocationUseCase,
+            setLocationUseCase: setLocationUseCase,
           ),
         ),
       ],
@@ -43,30 +63,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.purple,
-                Colors.deepPurple,
-              ],
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
-            ),
-          ),
-          height: double.infinity,
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              WeatherCard(),
-            ],
-          ),
-        ),
-      ),
+      home: LocationScreen(),
     );
   }
 }
