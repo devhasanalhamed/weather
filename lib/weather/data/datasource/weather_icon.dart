7cdc766199b292cfe820814936333b363525d373
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-11-25 06:20:59 +0300

    Create weather_icon.dart

---
+++ lib/weather/data/datasource/weather_icon.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class WeatherIcon {
+  Map<String, dynamic> weatherIcon = {
+    "clearSky": Icons.dynamic_feed,
+    "clearSkyNight": Icons.dynamic_feed,
+    "fewClouds": Icons.dynamic_feed,
+    "fewCloudsNight": Icons.dynamic_feed,
+    "scatteredClouds": Icons.dynamic_feed,
+    "scatteredCloudsNight": Icons.dynamic_feed,
+    "brokenClouds": Icons.dynamic_feed,
+    "brokenCloudsNight": Icons.dynamic_feed,
+    "showerRain": Icons.dynamic_feed,
+    "showerRainNight": Icons.dynamic_feed,
+    "rain": Icons.dynamic_feed,
+    "rainNight": Icons.dynamic_feed,
+    "thunderstorm": Icons.dynamic_feed,
+    "thunderstormNight": Icons.dynamic_feed,
+    "snow": Icons.dynamic_feed,
+    "snowNight": Icons.dynamic_feed,
+    "mist": Icons.dynamic_feed,
+    "mistNight": Icons.dynamic_feed,
+  };
+}
