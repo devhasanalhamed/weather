75b4d3b96115e7d7978225c9db9feb7794168b80
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-11-26 07:43:08 +0300

    Update constants.dart

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -2,5 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConstants {
   static String baseUrl = dotenv.env['BASE_URL']!;
+  static String weatherUrl = '$baseUrl/data/2.5/weather';
+  static String locationUrl = '$baseUrl/geo/1.0/direct';
   static String appKey = dotenv.env['API_KEY']!;
 }
