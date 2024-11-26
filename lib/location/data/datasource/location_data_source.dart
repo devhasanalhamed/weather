f985b7456d55d8419bfd39541e5aa3ac2756872e
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-11-26 07:43:11 +0300

    Update location_data_source.dart

--- lib/location/data/datasource/location_data_source.dart
+++ lib/location/data/datasource/location_data_source.dart
@@ -1,17 +1,24 @@
+import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:weather/core/utils/constants.dart';
 import 'package:weather/location/data/model/location_model.dart';
 import 'package:weather/location/domain/entities/location.dart';
 
 abstract class BaseLocationDataSource {
   Future<Location> getLocation();
+
   Future<void> setLocation(
     String? cityName,
     double? latitude,
     double? longitude,
   );
+
+  Future<List<LocationModel>> searchLocation(String query);
 }
 
 class LocationDataSource implements BaseLocationDataSource {
+  final dio = Dio();
+
   @override
   Future<Location> getLocation() async {
     final SharedPreferences sharedPreferences =
@@ -30,5 +37,25 @@ class LocationDataSource implements BaseLocationDataSource {
         await SharedPreferences.getInstance();
 
     await sharedPreferences.setString("city_name", cityName!);
+  }
+
+  @override
+  Future<List<LocationModel>> searchLocation(String query) async {
+    final limit = 5;
+    try {
+      final result = await dio.get(
+          '${AppConstants.locationUrl}?q=$query&limit=$limit&appid=${AppConstants.appKey}');
+      if (result.data != null) {
+        print(result.data);
+        List<LocationModel> cities = [];
+        for (var city in result.data) {
+          cities.add(LocationModel.fromJson(city));
+        }
+        return cities;
+      }
+      throw 'search location not find result';
+    } catch (error) {
+      throw 'search location';
+    }
   }
 }
