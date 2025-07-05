d1cf363fad296d93e9d73b16a8661c5fecfd55f1
Hasan Al-Hamed <therealhassan97@gmail.com>
2025-07-05 21:31:01 +0300

    delete old project

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
-import 'package:weather/location/data/datasource/location_data_source.dart';
-import 'package:weather/location/data/repository/location_repository.dart';
-import 'package:weather/location/domain/repository/base_location_repository.dart';
-import 'package:weather/location/domain/usecases/get_location_usecase.dart';
-import 'package:weather/location/domain/usecases/search_location_usecase.dart';
-import 'package:weather/location/domain/usecases/set_location_usecase.dart';
-import 'package:weather/location/presentation/controllers/location_provider.dart';
-import 'package:weather/location/presentation/presentation/location_screen.dart';
-import 'package:weather/weather/data/datasource/remote_data_source.dart';
-import 'package:weather/weather/data/repository/weather_repository.dart';
-import 'package:weather/weather/domain/repository/base_weather_repository.dart';
-import 'package:weather/weather/domain/usecases/get_weather_by_city_name.dart';
-import 'package:weather/weather/presentation/controllers/weather_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
 
-  BaseRemoteDataSource baseRemoteDataSource = RemoteDataSource();
-  BaseWeatherRepository baseWeatherRepository =
-      WeatherRepository(baseRemoteDataSource: baseRemoteDataSource);
-  GetWeatherByCityName getWeatherByCityName =
-      GetWeatherByCityName(baseWeatherRepository);
-
-  BaseLocationDataSource baseLocationDataSource = LocationDataSource();
-  BaseLocationRepository baseLocationRepository =
-      LocationRepository(baseLocationDataSource: baseLocationDataSource);
-  GetLocationUseCase getLocationUseCase =
-      GetLocationUseCase(baseLocationRepository: baseLocationRepository);
-  SetLocationUseCase setLocationUseCase =
-      SetLocationUseCase(baseLocationRepository: baseLocationRepository);
-  SearchLocationUsecase searchLocationUsecase =
-      SearchLocationUsecase(baseLocationRepository: baseLocationRepository);
-
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => WeatherProvider(
-            getWeatherByCityNameUseCase: getWeatherByCityName,
-          ),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => LocationProvider(
-            getLocationUseCase: getLocationUseCase,
-            setLocationUseCase: setLocationUseCase,
-            searchLocationUsecase: searchLocationUsecase,
-          ),
-        ),
-      ],
-      builder: (context, child) => MyApp(),
-    ),
+    MyApp(),
   );
 }
 
@@ -67,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LocationScreen(),
+      home: Center(),
     );
   }
 }
